80431a150d1a4ada4710b42364ee56662540b438
Arash Mehdipour <arash201221@gmail.com>
2021-03-14 19:12:36 +0330

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import 'materialize-css/dist/css/materialize.min.css';
+import 'materialize-css/dist/js/materialize.min.js'
+import './App.css';
+
+const App = () => {
+
+
+
+  return (
+    <h1>My App</h1>
+
+  )
+
+}
+
+export default App
